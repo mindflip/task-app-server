79b7bd0ea95f28f82d3c4b3084f3a9cdfcd15ccc
Eomy <hmu1212@hotmail.com>
2022-01-08 22:34:04 +0900

    [feat] implement post user in user router

---
+++ src/routers/user.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const User = require("../models/user");
+const router = new express.Router();
+
+router.post("/user", async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
