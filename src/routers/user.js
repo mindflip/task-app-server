65c5f68f5e341fbddef87d7613a786d148ff548e
Eomy <hmu1212@hotmail.com>
2022-01-09 23:23:47 +0900

    [feat] implement user login and auth in router

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,14 +1,35 @@
 const express = require("express");
+const auth = require("../middleware/auth");
 const User = require("../models/user");
 const router = new express.Router();
 
-router.post("/user", async (req, res) => {
+router.post("/users", async (req, res) => {
   const user = new User(req.body);
 
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (err) {
     res.status(400).send(err);
   }
 });
+
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (err) {
+    res.status(400).send();
+  }
+});
+
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
+});
+
+module.exports = router;
