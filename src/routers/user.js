bdd85a860df7c81f6dac20f3dd54af192bfe3743
Eomy <hmu1212@hotmail.com>
2022-01-10 11:02:33 +0900

    [feat] user avatar in router

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const multer = require("multer");
+const sharp = require("sharp");
 const auth = require("../middleware/auth");
 const User = require("../models/user");
 const router = new express.Router();
@@ -82,6 +84,50 @@ router.delete("/users/me", auth, async (req, res) => {
     res.send(req.user);
   } catch (err) {
     res.status(500).send();
+  }
+});
+
+const upload = multer({
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("File needs to be an image"));
+    }
+
+    cb(undefined, true);
+  },
+});
+
+// prettier-ignore
+router.post("/users/me/avatar", auth, upload.single("avatar"), async (req, res) => {
+  const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+  req.user.avatar = buffer;
+  await req.user.save();
+  res.send();
+}, (error, req, res, next) => {
+  res.status(400).send({ error: error.message });
+});
+
+router.delete("/users/me/avatar", auth, async (req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
+  res.send();
+});
+
+router.get("/users/:id/avatar", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user || !user.avatar) {
+      throw new Error("There is no user or user avatar");
+    }
+
+    res.set("Content-Type", "image/png");
+    res.send(user.avatar);
+  } catch (err) {
+    res.status(404).send();
   }
 });
 
