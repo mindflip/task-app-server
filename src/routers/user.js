2fe48f7eb36fe97bcf5bd3a3656e6c465d7e79f5
Eomy <hmu1212@hotmail.com>
2022-01-10 09:43:40 +0900

    [feat] delete user in router

--- src/routers/user.js
+++ src/routers/user.js
@@ -76,4 +76,13 @@ router.patch("/users/me", auth, async (req, res) => {
   }
 });
 
+router.delete("/users/me", auth, async (req, res) => {
+  try {
+    await req.user.remove();
+    res.send(req.user);
+  } catch (err) {
+    res.status(500).send();
+  }
+});
+
 module.exports = router;
