be0acb9a0cbf60a740d482a8db8f73c68217b17b
Eomy <hmu1212@hotmail.com>
2022-01-10 12:27:04 +0900

    [feat] build relationship using virtual between user and task

--- src/models/user.js
+++ src/models/user.js
@@ -59,6 +59,12 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+userSchema.virtual("tasks", {
+  ref: "Task",
+  localField: "_id",
+  foreignField: "owner",
+});
+
 userSchema.methods.toJSON = function () {
   const user = this;
   const userObject = user.toObject();
