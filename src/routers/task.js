df0244ff983c8b5e8a6a2f30264e1e5b9162137c
Eomy <hmu1212@hotmail.com>
2022-01-10 13:04:37 +0900

    [feat] patch task

--- src/routers/task.js
+++ src/routers/task.js
@@ -65,4 +65,33 @@ router.get("/tasks/:id", auth, async (req, res) => {
   }
 });
 
+router.patch("/tasks/:id", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["description", "completed"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const task = await Task.findOne({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    updates.forEach((update) => (task[update] = req.body[update]));
+    await task.save();
+    res.send(task);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
 module.exports = router;
