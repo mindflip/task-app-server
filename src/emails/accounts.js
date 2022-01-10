8cc952d5d52cadc54884f66ebff57c75e5ff319f
Eomy <hmu1212@hotmail.com>
2022-01-10 18:38:26 +0900

    [feat] install sendgrid, implement welcome and calcelation mailing

---
+++ src/emails/accounts.js
@@ -0,0 +1,36 @@
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const sendWelcomeEmail = async (email, name) => {
+  try {
+    await sgMail.send({
+      to: email,
+      from: "mindflip93@gmail.com",
+      subject: "Thanks for joining in!",
+      text: `Welcome to the app, ${name}. Let me know you get aling with the app`,
+    });
+    console.log("email test wc");
+  } catch (error) {
+    console.log(err);
+  }
+};
+
+const sendCancelationEmail = async (email, name) => {
+  try {
+    sgMail.send({
+      to: email,
+      from: "mindflip93@gmail.com",
+      subject: "Sorry to see you go!",
+      text: `Goodbye, ${name}. I hope to see you back sometime soon.`,
+    });
+    console.log("email test c");
+  } catch (error) {
+    console.log(err);
+  }
+};
+
+module.exports = {
+  sendWelcomeEmail,
+  sendCancelationEmail,
+};
